975060d1c2f034ff5a1fdb7fc936a3b383d841ff
chunyoung0518@gmail.com <chunyoung0518@gmail.com>
2021-09-02 10:15:15 +1000

    arrays

---
+++ objects.arrays.more/arrays.js
@@ -0,0 +1,17 @@
+var myArray = [];
+var daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday'];
+
+daysOfTheWeek[1];
+
+myArray = [1, 2, 3, 'String1', 'String2', true, false];
+
+myArray.length;
+
+myArray.push('last');
+myArray.pop();
+
+// clear the value but not remove
+delete myArray[2];
+
+// remove the value from the array
+myArray.splice(2, 1);
